8407810329e7668691b89b5c3fa778435698feb9
Miguel Mendosa <138393857+Mike37Jet@users.noreply.github.com>
2025-05-15 15:49:18 -0500

    15/5/2025
    Boton

--- Clase_9-5-2025/app/src/main/java/miguel/mendosa/ejemplo1/MainActivity4.kt
+++ Clase_9-5-2025/app/src/main/java/miguel/mendosa/ejemplo1/MainActivity4.kt
@@ -1,20 +1,35 @@
 package miguel.mendosa.ejemplo1
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+const val TAG = "MainActivity4"
 class MainActivity4 : AppCompatActivity() {
+    private lateinit var buttonPricing: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main4)
+        buttonPricing = findViewById(R.id.buttonPricing)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        buttonPricing.setOnClickListener {
+            Log.i(TAG, "Button Pricing clicked" + getString(R.string.buttonPricing_message))
+            Toast.makeText(this, getString(R.string.buttonPricing_message), Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun buttonBuyNowOnClic(view: View) {
+        Toast.makeText(this, "Compra realizada", Toast.LENGTH_SHORT).show()
     }
 }
