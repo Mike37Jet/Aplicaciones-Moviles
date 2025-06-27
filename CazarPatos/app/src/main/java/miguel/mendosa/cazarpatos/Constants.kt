1679165269c2fdebe4ae9f52ebe6f530af3ad348
Miguel Mendosa <138393857+Mike37Jet@users.noreply.github.com>
2025-06-27 15:48:50 -0500

    feat: Add file handling and encrypted preferences for user data storage

--- CazarPatos/app/src/main/java/miguel/mendosa/cazarpatos/Constants.kt
+++ CazarPatos/app/src/main/java/miguel/mendosa/cazarpatos/Constants.kt
@@ -1,3 +1,7 @@
 package miguel.mendosa.cazarpatos
 
 const val EXTRA_LOGIN = "EXTRA_LOGIN"
+
+const val LOGIN_KEY = "LOGIN_KEY"
+const val PASSWORD_KEY = "PASSWORD_KEY"
+const val SHAREDINFO_FILENAME = "mySharedInformation.dat"
